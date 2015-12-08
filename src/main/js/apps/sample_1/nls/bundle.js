e1c49725aea49d7a033eaa42e83d0b5847e9ca77
Matthias Stein <m.stein@conterra.de>
2015-12-08 10:28:44 +0100

    add new sample app

---
+++ src/main/js/apps/sample_1/nls/bundle.js
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2015 con terra GmbH (info@conterra.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+define({
+    root: {
+        application:{
+            infoTitle: "About this app",
+            description: "<p>This app is a demo to show how an ESRI Stream Layer can be visualized in map.apps. The used service displays the position, altitude, name and heading of satellites. If you watch carefully you can observe the satellites moving.</p><p><strong>Note:</strong> It might take a short while to display all satellites after the app has launched.</p><a href='http://developernetwork.conterra.de/downloads/bundles' target='blank'>Bundle download</a>"
+        },
+        service: {
+            darkbase: "Dark Basemap",
+            satellite: {
+                title: "Satellite Service",
+                infoTemplateTitle : "Satellite Name"
+            } 
+        }
+    },
+    de: true
+});
---
+++ src/main/js/apps/sample_1/nls/de/bundle.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2015 con terra GmbH (info@conterra.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+define({
+    application: {
+        infoTitle: "\u00DCber diese App",
+        description: "<p>Diese App zeigt, wie das Streamlayer Bundle verwendet werden kann um einen entsprechenden Arc GIS Dienst zu visualisieren. Der verwendete Dienst zeigt die H\u00F6he, Satelliten-Namen und dessen Kurs an. Wer genau hinsieht, kann die Bewegung der einzelnen Satelliten verfolgen.</p><p><strong>Achtung:</strong> Es kann eine Weile dauern bis die Satelliten Features auf der Karte dargestellt werden.</p><a href='http://developernetwork.conterra.de/downloads/bundles' target='blank'>Bundle download</a>"
+    },
+    service: {
+        darkbase: "Dunkle Basiskarte",
+        satellite: {
+            title: "Satelliten Dienst",
+            infoTemplateTitle: "Satelliten Name"
+
+        }
+    }
+});
