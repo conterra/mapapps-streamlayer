97c7b0c0b9bd363af37cd9fdb595c4d2715005c4
Matthias Stein <m.stein@conterra.de>
2016-02-16 16:50:57 +0100

    Update sample app

--- src/main/js/apps/sample_1/nls/bundle.js
+++ src/main/js/apps/sample_1/nls/bundle.js
@@ -20,7 +20,7 @@ define({
             description: "<p>This app is a demo to show how an ESRI Stream Layer can be visualized in map.apps. The used service displays the position, altitude, name and heading of satellites. If you watch carefully you can observe the satellites moving.</p><p><strong>Note:</strong> It might take a short while to display all satellites after the app has launched.</p><a href='http://developernetwork.conterra.de/downloads/bundles' target='blank'>Bundle download</a>"
         },
         service: {
-            darkbase: "Dark Basemap",
+            lightbase: "Dark Basemap",
             satellite: {
                 title: "Satellite Service",
                 infoTemplateTitle : "Satellite Name"
